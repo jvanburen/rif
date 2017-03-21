9a352c67b93c1ffec326689d30c7f4ba5b4bd59c
hackberrypi <redacted>
2017-03-21 05:02:13 -0400

    trainable classifier!

--- python/test_detect_resistor.py
+++ python/test_detect_resistor.py
@@ -1,18 +1,38 @@
 import numpy as np
 import cv2
 
-COLORS = 'bg black greenbg blue brown green red'.split(' ')
-color_samples = np.load('color_samples.npz')
-samples = []
-responses = []
-for i, color in enumerate(COLORS):
-  sample = color_samples[color]
-  response = np.array([i]*sample.shape[0], dtype=np.int32)
-  samples.append(sample)
-  responses.append(response)
-samples = np.concatenate(samples)
-responses = np.concatenate(responses)
+class Classifier:
+  COLORS = 'bg black greenbg blue brown green red'.split(' ')
+  def __init__(self, color_samples='color_samples.npz'):
+    color_samples = np.load(color_samples)
+    samples = []
+    responses = []
+    bgresponses = []
+    count = 0
+    for i, color in enumerate(Classifier.COLORS):
+      sample = color_samples[color]
+      samples.append(sample)
 
-bgclass = cv2.NormalBayesClassifier(samples, responses)
+      color_count = sample.shape[0]
+      count += color_count
+
+      response = np.array([i]*color_count, dtype=np.int32)
+      bgresponse = np.array([int(color != 'greenbg')] * color_count, dtype=np.int32)
+      responses.append(response)
+      bgresponses.append(bgresponse)
+
+    samples = np.concatenate(samples)
+    responses = np.concatenate(responses)
+    bgresponses = np.concatenate(bgresponses)
+
+    colorclass = bgclass = cv2.ml.NormalBayesClassifier_create()
+    colorclass.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+    bgclass = cv2.ml.NormalBayesClassifier_create()
+    bgclass.train(samples, cv2.ml.ROW_SAMPLE, bgresponses)
+
+    self.colorclass = colorclass
+    self.bgclass = bgclass
+
 
 
